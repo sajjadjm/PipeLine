83bfac0771a8bbd2dd741e636d47e965c4ee7032
sajjadjm <53228728+sajjadjm@users.noreply.github.com>
2019-12-26 02:52:54 -0800

    Project started

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello";
+    sf::RenderWindow win(sf::VideoMode(200, 200), "SFML Test");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (win.isOpen())
+    {
+        sf::Event event;
+        while (win.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                win.close();
+            }
+        }
+
+        win.clear();
+        win.draw(shape);
+        win.display();
+    }
+
+    return 0;
+}
